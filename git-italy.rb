2877d77cfcddc5b64e93b9d348fd19321104f032
ParviziAH <simone.parvizi@ah.nl>
2024-12-07 15:06:47 +0100

    update sha256

--- git-italy.rb
+++ git-italy.rb
@@ -2,7 +2,7 @@ class GitItaly < Formula
   desc "Git if it was made in italy"
   homepage "https://github.com/parviziAH/git-italy"
   url "https://github.com/parviziAH/git-italy/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "8eb0211921eb500b727d615d3da5c0aeb6c891e9876f23e1a50ad179352f1677"
   license "MIT"
 
   depends_on "python@3.9" # Or the version of Python your script requires
